e3c12174e2c2c4a2458573b5bee1c027d7427c81
Gleibson Alves <alvesgleibson@gmail.com>
2021-10-11 18:02:22 -0300

    Project Created

---
+++ app/src/main/java/com/alvesgleibson/messageappfamily/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alvesgleibson.messageappfamily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
