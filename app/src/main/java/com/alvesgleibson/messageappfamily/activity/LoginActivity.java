38c58227ed66e5e540316235ea537e7b93b45d76
Gleibson Alves <alvesgleibson@gmail.com>
2021-10-12 09:20:57 -0300

    Registration Interface

--- app/src/main/java/com/alvesgleibson/messageappfamily/activity/LoginActivity.java
+++ app/src/main/java/com/alvesgleibson/messageappfamily/activity/LoginActivity.java
@@ -2,11 +2,16 @@ package com.alvesgleibson.messageappfamily.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.alvesgleibson.messageappfamily.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView txtCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,12 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+
     }
+
+    public void cadastroTxt(View view){
+       startActivity( new Intent(this, CadastroActivity.class));
+    }
+
+
 }
