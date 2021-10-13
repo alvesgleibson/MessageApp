68bf29defcb17de821c1782425817ff9be43b9c2
Gleibson Alves <alvesgleibson@gmail.com>
2021-10-13 17:16:34 -0300

    Add Menu

--- app/src/main/java/com/alvesgleibson/messageappfamily/activity/MainActivity.java
+++ app/src/main/java/com/alvesgleibson/messageappfamily/activity/MainActivity.java
@@ -1,9 +1,14 @@
 package com.alvesgleibson.messageappfamily.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.alvesgleibson.messageappfamily.R;
 
@@ -16,8 +21,38 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("MessageAPP");
-        setSupportActionBar( toolbar );
+        setSupportActionBar(toolbar);
 
+    }
 
+    //Criar Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Selecionar opção menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.menu_pesquisa:
+                Toast.makeText(this, "Pesquisa", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_conficuracao:
+                Toast.makeText(this, "Configuração", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_sair:
+                Toast.makeText(this, "Sair", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
