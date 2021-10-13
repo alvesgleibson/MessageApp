f6af383f5eaeaec36497302f2e353ea522c7bffc
Gleibson Alves <alvesgleibson@gmail.com>
2021-10-13 11:44:13 -0300

    Add toolbar

--- app/src/main/java/com/alvesgleibson/messageappfamily/activity/MainActivity.java
+++ app/src/main/java/com/alvesgleibson/messageappfamily/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.alvesgleibson.messageappfamily.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("MessageAPP");
+        setSupportActionBar( toolbar );
+
+
     }
 }
