4892030c9087b2873bf467d322047c536f5a148b
Gleibson Alves <alvesgleibson@gmail.com>
2021-10-21 21:23:15 -0300

    Added RecycleView ListaContatos

---
+++ app/src/main/java/com/alvesgleibson/messageappfamily/adapter/ListaContatoAdapter.java
@@ -0,0 +1,73 @@
+package com.alvesgleibson.messageappfamily.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.alvesgleibson.messageappfamily.R;
+import com.alvesgleibson.messageappfamily.model.Usuario;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ListaContatoAdapter extends RecyclerView.Adapter<ListaContatoAdapter.MyViewHolder> {
+
+    private List<Usuario> usuarioList;
+    private Context context;
+
+    public ListaContatoAdapter(List<Usuario> usuarioList, Context context) {
+        this.usuarioList = usuarioList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_lista_contato, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Usuario usuario = usuarioList.get( position );
+
+
+        holder.txtNome.setText( usuario.getName() );
+        holder.txtEmail.setText( usuario.getEmail() );
+        if (usuario.getFoto() != null) {
+
+            Glide.with(context).load(Uri.parse(usuario.getFoto())).into(holder.imgPerfil);
+
+        }else holder.imgPerfil.setImageResource(R.drawable.padrao);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return usuarioList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private CircleImageView imgPerfil;
+        private TextView txtNome, txtEmail;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imgPerfil = itemView.findViewById(R.id.imgContatoPerfil);
+            txtNome =  itemView.findViewById(R.id.txtContatosNome);
+            txtEmail =  itemView.findViewById(R.id.txtContodosEmail);
+
+        }
+    }
+}
--- app/src/main/java/com/alvesgleibson/messageappfamily/fragment/ContatoFragment.java
+++ app/src/main/java/com/alvesgleibson/messageappfamily/fragment/ContatoFragment.java
@@ -1,16 +1,41 @@
 package com.alvesgleibson.messageappfamily.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.alvesgleibson.messageappfamily.R;
+import com.alvesgleibson.messageappfamily.adapter.ListaContatoAdapter;
+import com.alvesgleibson.messageappfamily.helper.Base64Costum;
+import com.alvesgleibson.messageappfamily.helper.UsuarioFirebase;
+import com.alvesgleibson.messageappfamily.model.Usuario;
+import com.alvesgleibson.messageappfamily.setting.SettingInstanceFirebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.EventListener;
+import java.util.List;
 
 public class ContatoFragment extends Fragment {
+
+    private List<Usuario> usuarioList = new ArrayList<>();
+    private RecyclerView recyclerViewContatos;
+    private ListaContatoAdapter listaContatoAdapter;
+    private ValueEventListener eventListener;
+    private  DatabaseReference databaseReferenceParaRecycleView;
+
+
 
     public ContatoFragment() {
     }
@@ -18,7 +43,67 @@ public class ContatoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contato, container, false);
+        View view = inflater.inflate(R.layout.fragment_contato, container, false);
+
+
+        recyclerViewContatos = view.findViewById(R.id.rvContato);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager( getActivity() );
+
+
+        recyclerViewContatos.setLayoutManager(layoutManager );
+
+        recyclerViewContatos.setHasFixedSize(true);
+
+        listaContatoAdapter = new ListaContatoAdapter(usuarioList, getActivity());
+
+        recyclerViewContatos.setAdapter( listaContatoAdapter);
+        buscarUsuariosFirebase();
+
+        return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    databaseReferenceParaRecycleView.removeEventListener( eventListener );
+
+    }
+
+    private void buscarUsuariosFirebase() {
+        String email = Base64Costum.decodeBase64(UsuarioFirebase.getIdentificadorUsuario());
+
+        databaseReferenceParaRecycleView = SettingInstanceFirebase.getDatabaseReference().child("Users");
+
+        eventListener = databaseReferenceParaRecycleView.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot usuarioSnapshot : snapshot.getChildren()){
+
+                    Usuario usuario1 = usuarioSnapshot.getValue( Usuario.class );
+                    usuarioList.add( usuario1 );
+
+
+                }
+                listaContatoAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+    }
+
+
+
 }
